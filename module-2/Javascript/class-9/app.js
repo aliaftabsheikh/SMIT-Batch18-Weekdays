56936bc1a556f42dfc8b19546f9728547c04be54
aliaftabsheikh <aliaftabsheikh1@gmail.com>
2025-12-29 19:02:38 +0500

    'class-9'

---
+++ module-2/Javascript/class-9/app.js
@@ -0,0 +1,45 @@
+var cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar", "Hyderabad"];
+
+// var citiesLength = cities.length;
+
+// console.log("CitiesLength:", citiesLength);
+
+
+// console.log("City :", cities[citiesLength - 2]);
+
+// for(var i = 0; i < cities.length; i++){
+//     console.log("City : ", cities[i]);
+// }
+
+// cities[6] = "Faisalabad";
+
+// cities[1] = "Multan";
+
+//! SHIFT
+
+// var removedCity = cities.shift();
+// console.log("Removed City:", removedCity);
+
+
+//! PUSH
+
+// cities.push("Faisalabad", "Multan");
+
+//! POP
+
+// var removedCity = cities.pop();
+// console.log("Removed City:", removedCity);
+
+//! UNSHIFT
+
+// cities.unshift("Sialkot", "Gujranwala");
+
+//! SPLICE
+
+cities.splice(2, 1, "Sargodha");
+cities.splice(0, 0, "Bahawalpur");
+
+console.log(cities);
+
+
+// console.log(cities[6]);
