8bcf6662f471116d71e03190d923b69b0a442591
aliaftabsheikh <aliaftabsheikh1@gmail.com>
2026-01-30 19:01:20 +0500

    'class-20'

---
+++ module-2/Javascript/class-20/app.js
@@ -0,0 +1,36 @@
+// function greet(name){
+//     alert('Hello ' + name);
+// }
+
+
+// function onMouseEnter(element){
+//     element.style.color = 'blue';
+// }
+
+// function onMouseLeave(element){
+//     element.style.color = 'green';
+// }
+
+
+
+
+var num1 = 10
+var num2 = 20
+
+add(num1, num2)
+
+var sum = 10
+
+function add(a, b) {
+    console.log(a + b);
+
+    sum = 20
+
+    console.log(sum);
+}
+
+// console.log(sum);
+
+
+// function calling/invoke
+
