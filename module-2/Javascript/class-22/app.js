b7e3e65eee76d5601a679f3405642526e9340009
aliaftabsheikh <aliaftabsheikh1@gmail.com>
2026-02-04 18:50:17 +0500

    'class-22'

---
+++ module-2/Javascript/class-22/app.js
@@ -0,0 +1,32 @@
+//! Function is a block of code that performs a specific task when we call or invoke it.
+
+
+// function definition / statement
+
+// function greet(name, quote){
+//     var message = "Hello " + name + ", " + quote + "!";
+//     return message;
+// }
+
+// // function invocation / call
+
+// var message1 = greet("Chippa", " Man") 
+// var message2 = greet("Chippa Sahab", " That's it") 
+
+// console.log(message1);
+// console.log(message2);
+
+
+var fruits = ["Apple", "Banana", "Mango", "Orange", "Strawberry", "Pineapple", "Watermelon", "Kiwi", "Papaya"];
+
+// fruits.pop()
+// fruits.push("Grapes");
+
+// fruits.splice(0, 0, "Pineapple");
+
+
+var slicedFruits = fruits.slice(6, 8)
+
+console.log(slicedFruits);
+
+console.log(fruits);
