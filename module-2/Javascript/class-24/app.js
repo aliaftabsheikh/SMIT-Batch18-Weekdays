f7042353008aff4f62ad6b689762745b5b0aad2e
aliaftabsheikh <aliaftabsheikh1@gmail.com>
2026-02-09 19:05:45 +0500

    'class-24'

---
+++ module-2/Javascript/class-24/app.js
@@ -0,0 +1,11 @@
+console.log(this)
+
+
+// DOM 
+// DOM TREE
+// Document child Nodes
+// document.getElementById() 
+// document.getElementsByClassName()
+// document.getElementsByTagName()
+// document.querySelector()
+// document.querySelectorAll() 
