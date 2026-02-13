908af71f2a10d7766e70ef58f403646cebb0eac5
aliaftabsheikh <aliaftabsheikh1@gmail.com>
2026-02-13 19:00:21 +0500

    'class-26'

---
+++ module-2/Javascript/class-26/app.js
@@ -0,0 +1,28 @@
+var todoList = document.getElementById('todo-list');
+
+function addTodo(){
+    var todoInput = document.getElementById('input-field');
+    var todoText = todoInput.value;
+
+    if(todoText === ""){
+        alert("Please enter a todo item.");
+        return;
+    }
+
+    var todo = `<div>
+    <p class="todo" id="todo-${Date.now()}">
+        ${todoText}
+    </p>
+
+    <button onclick="deleteTodo('todo-${Date.now()}')">Delete !</button>
+    </div>`;
+
+
+
+    todoList.innerHTML += todo;
+
+    todoInput.value = "";
+
+
+
+}
