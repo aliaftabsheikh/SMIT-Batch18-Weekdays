5a5221d5a18b92438692422cdec8e11f10b521c4
aliaftabsheikh <aliaftabsheikh1@gmail.com>
2026-06-12 17:52:03 +0500

    'class-26'

---
+++ module-2/Javascript/advanced-js/class-26/app.js
@@ -0,0 +1,44 @@
+import {} from "./calculator.js";
+
+//! Optional chaining 
+
+
+// const user = undefined
+
+
+
+
+// // console.log(user?.name);
+
+// console.log(user?.cnic);
+// // console.log(user?.cnic);    
+
+
+// const data = undefined
+
+// const result = data?.map((item)=>{
+//     return item 
+// })
+
+// console.log(result);
+
+console.log(add(5, 3));
+console.log(subtract(5, 3));
+console.log(multiply(5, 3));
+console.log(divide(5, 3));
+
+
+// const name = "Ali"
+// const age = 25
+
+// const person = {
+//     name,
+//     age 
+// }
+
+// console.log(person)
+
+
+// COMMONJS / MODULE JS 
+
+// OOP (object oriented programming)
